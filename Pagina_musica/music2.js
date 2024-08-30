15fe1e7f01710fc75d7c6333e33781c6c99b0bc6
JoaopedroSA-cmd <antunesjoaopedro3@gmail.com>
2024-08-30 19:49:00 -0300

    v9.9

--- Pagina_musica/music2.js
+++ Pagina_musica/music2.js
@@ -1,43 +1,43 @@
-// let controleEsquerdo = document.getElementById("controleEsquerdo");
-// let controleDireito = document.getElementById("controleDireito");
-// let elementoAudio = document.querySelector(".music_ordem1");
-// let tempo;
+let controleEsquerdo = document.getElementById("controleEsquerdo");
+let controleDireito = document.getElementById("controleDireito");
+let elementoAudio = document.querySelector(".music_ordem1");
+let tempo;
 
-// let localizacao = 0; // posição inicial
-// const passo = 25; // pixels por intervalo
+let localizacao = 0; // posição inicial
+const passo = 25; // pixels por intervalo
 
-// function deslocarParaEsquerda() {
-//   clearInterval(tempo);
-//   tempo = setInterval(() => {
-//     localizacao -= passo;
-//     if (localizacao < -elementoAudio.offsetWidth) { // se o elemento saiu do limite esquerdo
-//       localizacao = window.innerWidth; // move o elemento para o limite direito
-//     }
-//     elementoAudio.style.left = localizacao + "px";
-//   }, 20);
-// }
+function deslocarParaEsquerda() {
+  clearInterval(tempo);
+  tempo = setInterval(() => {
+    localizacao -= passo;
+    if (localizacao < -elementoAudio.offsetWidth) { // se o elemento saiu do limite esquerdo
+      localizacao = window.innerWidth; // move o elemento para o limite direito
+    }
+    elementoAudio.style.left = localizacao + "px";
+  }, 20);
+}
 
-// function deslocarParaDireita() {
-//   clearInterval(tempo);
-//   tempo = setInterval(() => {
-//     localizacao += passo;
-//     if (localizacao > window.innerWidth) { // se o elemento saiu do limite direito
-//       localizacao = -elementoAudio.offsetWidth; // move o elemento para o limite esquerdo
-//     }
-//     elementoAudio.style.left = localizacao + "px";
-//   }, 20);
-// }
+function deslocarParaDireita() {
+  clearInterval(tempo);
+  tempo = setInterval(() => {
+    localizacao += passo;
+    if (localizacao > window.innerWidth) { // se o elemento saiu do limite direito
+      localizacao = -elementoAudio.offsetWidth; // move o elemento para o limite esquerdo
+    }
+    elementoAudio.style.left = localizacao + "px";
+  }, 20);
+}
 
-// function pararDeslocamento() {
-//   clearInterval(tempo);
-// }
+function pararDeslocamento() {
+  clearInterval(tempo);
+}
 
-// controleEsquerdo.addEventListener("mousedown", deslocarParaEsquerda);
-// controleEsquerdo.addEventListener("mouseup", pararDeslocamento);
-// controleDireito.addEventListener("mousedown", deslocarParaDireita);
-// controleDireito.addEventListener("mouseup", pararDeslocamento);
+controleEsquerdo.addEventListener("mousedown", deslocarParaEsquerda);
+controleEsquerdo.addEventListener("mouseup", pararDeslocamento);
+controleDireito.addEventListener("mousedown", deslocarParaDireita);
+controleDireito.addEventListener("mouseup", pararDeslocamento);
 
-// controleEsquerdo.addEventListener("touchstart", deslocarParaEsquerda);
-// controleEsquerdo.addEventListener("touchend", pararDeslocamento);
-// controleDireito.addEventListener("touchstart", deslocarParaDireita);
-// controleDireito.addEventListener("touchend", pararDeslocamento);
+controleEsquerdo.addEventListener("touchstart", deslocarParaEsquerda);
+controleEsquerdo.addEventListener("touchend", pararDeslocamento);
+controleDireito.addEventListener("touchstart", deslocarParaDireita);
+controleDireito.addEventListener("touchend", pararDeslocamento);
